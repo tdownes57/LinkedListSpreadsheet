b3cb95faac0e8f14afa1ec2a1d50c5e85a6b9a39
Thomas C. Downes <tomdownes1@gmail.com>
2026-02-04 18:13:10 -0800

    Add files via upload

---
+++ TestingRSCSpreadsheet2/FormTestGrid2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingRSCSpreadsheet2
+{
+    public partial class FormTestGrid2 : Form
+    {
+        public FormTestGrid2()
+        {
+            InitializeComponent();
+        }
+
+        private void userOperationPartial1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
